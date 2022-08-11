ffd5be58b847c0431b3c4b317489fb392e1a9b02
Alexc0217 <alex.costa.moura@outlook.com>
2022-08-10 22:09:36 -0300

    sending object to front end

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask("projeto")
 
 @app.route("/")
 def ola_mundo():
-
-    return "Olá Mundo! Esse é meu primeiro projeto Flask!"
+    nome = "Alex Moura"
+    produtos = [
+        {"nome": "tomate", "preco": "12,99"},
+        {"nome": "valorant points", "preco": "42,90"}
+    ]
+    return render_template("jinga2.html", n = nome, p = produtos), 200
 
 app.run()
